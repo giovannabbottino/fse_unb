35469ccfee45e0095f08239f25081e1a49dda532
Felipe Agustini <felipeboccardi@hotmail.com>
2022-09-21 22:12:58 -0300

    Configurando LED PWM
    Co-authored-by: giovannabbottino < giovannabbottino@gmail.com>

--- trabalho_3/include/led.h
+++ trabalho_3/include/led.h
@@ -1,7 +1,6 @@
 #ifndef __LED_H__
 #define __LED_H__
 
-void led_setup();
 void led_set_state(int state);
 
 int led_get_state();
--- trabalho_3/src/led.c
+++ trabalho_3/src/led.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
+#include "driver/ledc.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
-#include "driver/ledc.h"
 #include "led.h"
-#include "esp_log.h"
-
+#include <stdbool.h>
 
 #define TAG "LED"
 
@@ -19,55 +16,54 @@
 extern struct memoria *data;
 
 bool g_setup_led = false;
-void led_setup(){
 
-    //Configure timer 0
-    ledc_timer_config_t ledc_timer = {
-        .duty_resolution    = LEDC_TIMER_8_BIT,
-        .freq_hz            = 100,
-        .speed_mode         = LEDC_HIGH_SPEED_MODE,
-        .timer_num          = LEDC_TIMER_0
+static void led_setup(void){
+    // Configuração do Timer
+    ledc_timer_config_t timer_config = {
+    .speed_mode = LEDC_LOW_SPEED_MODE,
+    .duty_resolution = LEDC_TIMER_8_BIT,
+    .timer_num = LEDC_TIMER_0,
+    .freq_hz = 1000,
+    .clk_cfg = LEDC_AUTO_CLK
     };
-    ledc_timer_config(&ledc_timer);
+    ledc_timer_config(&timer_config);
 
-    //Configure channels
-    ledc_channel_config_t ledc_channel = {
-        .channel    = LEDC_CHANNEL_0,
-        .duty       = 0,
-        .hpoint     = 0,
-        .gpio_num   = 2,
-        .intr_type  = LEDC_INTR_DISABLE,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .timer_sel  = LEDC_TIMER_0
+    // Configuração do Canal
+    ledc_channel_config_t channel_config = {
+    .gpio_num = 2,
+    .speed_mode = LEDC_LOW_SPEED_MODE,
+    .channel = LEDC_CHANNEL_0,
+    .timer_sel = LEDC_TIMER_0,
+    .duty = 0,
+    .hpoint = 0
     };
-    ledc_channel_config(&ledc_channel);
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, data->led);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-
+    ledc_channel_config(&channel_config);
     g_setup_led = true;
 }
 
 void led_set_state(int state){
+    state = 2.55 * state;
     if(!g_setup_led){
         led_setup();
     }
     ESP_LOGI(TAG, "LED: %d", state);
 
-    if (state <= data.led){
-        for (int i = data.led; i < state; i++){
+    if (state > data->led){
+        for (int i = data->led; i < state; i++){
             ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, i);
             ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
             vTaskDelay(10 / portTICK_PERIOD_MS);
         }
     } else {
-        for (int i = state; i > data.led; i--){
-            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, i);
-            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
-            vTaskDelay(10 / portTICK_PERIOD_MS);
+        for (int i = data->led; i >= state; i--){ 
+         ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, i); 
+         ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+         vTaskDelay(10 / portTICK_PERIOD_MS);
         }
     }
+    data->led = state;
 }
 
 int led_get_state(){
-    return data.led;
+    return data->led;
 }
